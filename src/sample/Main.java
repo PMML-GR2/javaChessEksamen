a5defba3ee2bf07008eb026d6845b5a82dd4ded5
Mikael Rønnevik <mikaelron@gmail.com>
2020-05-03 16:41:07 +0200

    deleted some shit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,18 +13,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
-
         BorderPane mikaelPane = new BorderPane();
-
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         BorderPane bpane = new BorderPane();
         BorderPane Mirsa = new BorderPane();
 
-
         Scene scene = new Scene(bpane, 650, 500);
-
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(scene);
         primaryStage.show();
